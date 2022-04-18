c8b31eb346d5dac15415345036024abab9137166
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-18 21:39:29 +0530

    tables styling done

--- index.js
+++ index.js
@@ -6,19 +6,23 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const changeTable = document.querySelector(".change-table");
 const cashGivenCal = document.querySelector(".cash-given-calc");
 const nextBtn = document.querySelector(".next-btn");
+// const returnAmt=document.querySelector(".return-amt");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
-cashGivenCal.style.display = "none";
-changeTable.style.display = "none";
+displayStyling();
 nextBtn.addEventListener("click", function validateBillAmt() {
     console.log(typeof billAmt.value)
     if (Number(billAmt.value) <= 0) {
         showMessage('invalid bill amount');
-        cashGivenCal.style.display = "none";
+        displayStyling();
     } else {
         cashGivenCal.style.display = 'block';
+
     }
 });
-
+function displayStyling(){
+    cashGivenCal.style.display = "none";
+    changeTable.style.display = "none";
+}
 function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
@@ -27,6 +31,7 @@ checkBtn.addEventListener("click", function checkCashGiven() {
     if (Number(cashGiven.value) >= Number(billAmt.value)) {
         changeTable.style.display = "block"
         const amtToBeReturned = cashGiven.value - billAmt.value;
+        message.innerText=`Amount to be return is : ${amtToBeReturned}`;
         calculateChange(amtToBeReturned);
     } else {
         showMessage("Cash given is insufficient");
