73b7f77334fb9dd1b54381976ff0290d3fd3ef3e
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-28 10:07:30 +0530

    refactor the css files

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const changeTable = document.querySelector(".change-table");
 const cashGivenCal = document.querySelector(".cash-given-calc");
 const nextBtn = document.querySelector(".next-btn");
-// const returnAmt=document.querySelector(".return-amt");
+
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 displayStyling();
 nextBtn.addEventListener("click", function validateBillAmt() {
-    console.log(typeof billAmt.value)
     if (Number(billAmt.value) <= 0) {
         showMessage('invalid bill amount');
         displayStyling();
